c79e64451072364076ea1642840366ca01aa04e1
geek-tubash <58830572+geek-tubash@users.noreply.github.com>
2025-04-26 20:48:30 +0100

    Update urls.py

--- mediaapp/urls.py
+++ mediaapp/urls.py
@@ -1,14 +1,16 @@
+# multimedia_project/urls.py
+
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
-from .views import upload_media, media_list, edit_media, delete_media, migrate_now
+from mediaapp.views import upload_media, media_list, edit_media, delete_media, run_migrations  # ✅
 
 urlpatterns = [
     path('upload/', upload_media, name='upload_media'),
     path('', media_list, name='media_list'),
     path('edit/<int:pk>/', edit_media, name='edit_media'),
     path('delete/<int:pk>/', delete_media, name='delete_media'),
+    path('runnow/', run_migrations),  # ✅ for migrations
 ]
 
 # Serve media files during development
